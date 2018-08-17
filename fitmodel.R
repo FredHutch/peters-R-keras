ef688d75e85dd6e113d3714088e0ef2447e16758
Minta821 <minta82@gmail.com>
2018-08-17 15:54:49 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -10,6 +10,7 @@ y_train=x_train[,1]
 
 x_train=data.matrix(x_train[,-1])
 
+######test1
 x_test=fread('x_test.csv')
 
 x_test=data.frame(x_test)
@@ -17,6 +18,24 @@ x_test=data.frame(x_test)
 y_test=x_test[,1]
 
 x_test=data.matrix(x_test[,-1])
+
+########test2
+test_uk=fread('test_uk.csv')
+
+test_uk=data.frame(test_uk)
+
+y_uk=test_uk[,1]
+
+test_uk=data.matrix(test_uk[,-1])
+###########test3
+
+test_plco=fread('test_plco.csv')
+
+test_plco=data.frame(test_plco)
+
+y_plco=test_plco[,1]
+
+test_plco=data.matrix(test_plco[,-1])
 
 
 model <- keras_model_sequential()
@@ -48,14 +67,16 @@ parallel_model %>%   compile(
   )
 
 #score1=c()
-#score=c()
+
 
 #for(i in c(20,40,60,80,100,120,140,160)){
-parallel_model %>% fit(x_train, y_train, epochs = 20, batch_size = 2823)
+parallel_model %>% fit(x_train, y_train, epochs = 20, batch_size = 128)
 #score1 = c(score1,parallel_model %>% evaluate(x_train, y_train, batch_size=128))
 score = parallel_model %>% evaluate(x_test, y_test, batch_size=128)
+score1 = parallel_model %>% evaluate(test_uk, y_uk, batch_size=128)
+score2 = parallel_model %>% evaluate(test_plco, y_plco, batch_size=128)
 # }
-#ss=cbind(score1,score)
+ss=cbind(score,score1,score2)
 #score = parallel_model %>% predict(x_test, batch_size=128)
-fwrite(data.frame(score),'score.csv')
+fwrite(data.frame(ss),'score.csv')
 
