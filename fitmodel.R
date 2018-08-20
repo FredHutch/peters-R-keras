ef15f81719c0c20e43241c0a4f3b6e0086b3cea9
Minta821 <minta82@gmail.com>
2018-08-20 10:23:22 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -12,23 +12,23 @@ x_train=data.matrix(x_train[,-1])
 x_train=x_train[,1:30000]
 
 ######test1
-x_test=fread('x_test.csv')
+#x_test=fread('x_test.csv')
 
-x_test=data.frame(x_test)
+#x_test=data.frame(x_test)
 
-y_test=x_test[,1]
+#y_test=x_test[,1]
 
 
-x_test=data.matrix(x_test[,-1])
-x_test=x_test[,1:30000]
+#x_test=data.matrix(x_test[,-1])
+#x_test=x_test[,1:30000]
 
 ########test2
-#test_uk=fread('test_uk.csv')
+test_uk=fread('test_uk.csv')
 
-#test_uk=data.frame(test_uk)
+test_uk=data.frame(test_uk)
 
-#y_uk=test_uk[,1]
-#test_uk=data.matrix(test_uk[,-1])
+y_uk=test_uk[,1]
+test_uk=data.matrix(test_uk[,-1])
 ###########test3
 
 #test_plco=fread('test_plco.csv')
@@ -72,14 +72,14 @@ parallel_model %>%   compile(
 
 
 #for(i in c(20,40,60,80,100,120,140,160)){
-parallel_model %>% fit(x_train, y_train, epochs = 20, batch_size = 128, class_weight={1:1, 0:30})
+parallel_model %>% fit(x_train, y_train, epochs = 20, batch_size = 128)
 #score1 = c(score1,parallel_model %>% evaluate(x_train, y_train, batch_size=128))
 #score = parallel_model %>% evaluate(x_test, y_test, batch_size=128)
-score = parallel_model %>% predict(x_test, batch_size=128)
-#score1 = parallel_model %>% evaluate(test_uk, y_uk, batch_size=128)
+#score = parallel_model %>% predict(x_test, batch_size=128)
+score1 = parallel_model %>% evaluate(test_uk, y_uk, batch_size=128)
 #score2 = parallel_model %>% evaluate(test_plco, y_plco, batch_size=128)
 # }
 #ss=cbind(score,score1,score2)
 #score = parallel_model %>% predict(x_test, batch_size=128)
-fwrite(data.frame(score),'score.csv')
+fwrite(data.frame(score1),'score.csv')
 
