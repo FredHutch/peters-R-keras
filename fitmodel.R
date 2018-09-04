52caaa7b50a526d37a06932eceb9d841a90386c2
Minta821 <minta82@gmail.com>
2018-09-04 14:50:30 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -86,11 +86,11 @@ parallel_model %>%   compile(
 
 
 #for(i in c(20,40,60,80,100,120,140,160)){
-parallel_model %>% fit(x_train, y_train, epochs = 20, batch_size = 128)
+parallel_model %>% fit(x_train, y_train, epochs = 20, batch_size = 258)
 #score1 = c(score1,parallel_model %>% evaluate(x_train, y_train, batch_size=128))
 #score = parallel_model %>% evaluate(x_test, y_test, batch_size=128)
 #score = parallel_model %>% predict(x_test, batch_size=128)
-score1 = parallel_model %>% predict(test_uk, y_uk, batch_size=128)
+score1 = parallel_model %>% predict(test_uk, y_uk, batch_size=258)
 #score2 = parallel_model %>% evaluate(test_plco, y_plco, batch_size=128)
 # }
 #ss=cbind(score,score1,score2)
