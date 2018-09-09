f971c0b2b1045eaffa985ea3fd25ef5bae6483cf
Minta821 <minta82@gmail.com>
2018-09-09 16:08:24 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -9,7 +9,7 @@ x_train=data.frame(x_train)
 y_train=x_train[,1]
 
 x_train=data.matrix(x_train[,-1])
-x_train=x_train[,1:30000]
+x_train=scale(x_train[,1:30000])
 
 get.gpu.count <- function() {
   out <- system2("nvidia-smi", "-L", stdout=TRUE)
@@ -34,7 +34,7 @@ test_uk=data.frame(test_uk)
 
 y_uk=test_uk[,1]
 test_uk=data.matrix(test_uk[,-1])
-test_uk=test_uk[,1:30000]
+test_uk=scale(test_uk[,1:30000])
 ###########test3
 
 #test_plco=fread('test_plco.csv')
