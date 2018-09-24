899fc51859a4eae56540c63054512e05a3eddbb8
Minta821 <minta82@gmail.com>
2018-09-24 13:27:55 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -115,7 +115,7 @@ max(history.reg$metrics$val_acc)
 #rm(model.reg)
 
 #for(i in c(20,40,60,80,100,120,140,160)){
-parallel_model %>% fit(scale(x_train[,1:15000]), y_train, epochs = 20, batch_size = 258)
+#parallel_model %>% fit(scale(x_train[,1:15000]), y_train, epochs = 20, batch_size = 258)
 #score1 = c(score1,parallel_model %>% evaluate(x_train, y_train, batch_size=128))
 #score = parallel_model %>% evaluate(x_test, y_test, batch_size=128)
 score = parallel_model %>% predict(scale(test_uk[,1:15000]), batch_size=128)
