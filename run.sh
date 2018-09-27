8880e21f0dbc25bc2188637e27e39539aa6be024
Minta821 <minta82@gmail.com>
2018-09-27 14:44:47 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,13 +21,13 @@ aws s3 cp $X_TRAIN_S3_URL ./x_train.csv
 #aws s3 cp $X_TEST_S3_URL ./x_test.csv
 aws s3 cp $X_TEST_uk_S3_URL ./test_uk.csv
 #aws s3 cp $X_TEST_plco_S3_URL ./test_plco.csv
-
+aws s3 cp model_reg.hdf5 $MODEL_OUTPUT_S3_URL
 if [ -z ${USE_PYTHON+x} ];
 then
     echo USE_PYTHON is not set, using R
     time R -f fitmodel.R
-    aws s3 cp history.reg.pdf $PDF_OUTPUT_S3_URL
-    aws s3 cp model_reg.hdf5 $MODEL_OUTPUT_S3_URL
+    #aws s3 cp history.reg.pdf $PDF_OUTPUT_S3_URL
+    #aws s3 cp model_reg.hdf5 $MODEL_OUTPUT_S3_URL
 else
     echo USE_PYTHON is set, using python
     time python fitmodel.py
