fe291d5ac1a7e0d08931ba5e361da9887261229d
Minta821 <minta82@gmail.com>
2018-10-14 19:36:07 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -61,6 +61,8 @@ model %>%
  layer_dropout(rate = 0) %>%
  layer_dense(units = 250, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh') %>%
  layer_dropout(rate = 0) %>%
+ layer_dense(units = 150, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh') %>%
+ layer_dropout(rate = 0) %>%
   #layer_dense(units = 150, kernel_regularizer = regularizer_l2(0.001), activation = 'sigmoid') %>%
   #layer_dropout(rate = 0.3) %>%
   #layer_dense(units = 150, kernel_regularizer = regularizer_l2(0.001), activation = 'sigmoid') %>%
