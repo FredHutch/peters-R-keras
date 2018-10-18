7e461dc1379fdbec7c0b161a6a6eed04faea185a
Minta821 <minta82@gmail.com>
2018-10-18 09:50:08 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -54,15 +54,15 @@ fscore=matrix(0,length(y_uk),1)
 ###MLP NN
 model %>%
  layer_dense(units = 2000, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh', input_shape = c(50000)) %>%
- layer_dropout(rate = 0) %>%
+ layer_dropout(rate = 0.2) %>%
  layer_dense(units = 1000, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh') %>%
- layer_dropout(rate = 0) %>%
+ layer_dropout(rate = 0.2) %>%
  layer_dense(units = 500, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh') %>%
-layer_dropout(rate = 0) %>%
+layer_dropout(rate = 0.2) %>%
  layer_dense(units = 250, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh') %>%
- layer_dropout(rate = 0) %>%
+ layer_dropout(rate = 0.2) %>%
  layer_dense(units = 150, kernel_regularizer = regularizer_l2(0.01), activation = 'tanh') %>%
- layer_dropout(rate = 0) %>%
+ layer_dropout(rate = 0.2) %>%
  layer_dense(units = 1, activation = 'sigmoid') 
 
 ###CNN
