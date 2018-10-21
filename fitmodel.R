9608081dc5423069d88d3df242efc2deb862fb09
Minta821 <minta82@gmail.com>
2018-10-20 18:10:25 -0700

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -53,7 +53,7 @@ fscore=matrix(0,length(y_uk),1)
 #for(i in 1:4){
 ###MLP NN
 model %>%
- layer_dense(units = 2000, kernel_regularizer = regularizer_l2(0.001), activation = 'tanh', input_shape = c(50000)) %>%
+ layer_dense(units = 5000, kernel_regularizer = regularizer_l2(0.001), activation = 'tanh', input_shape = c(30000)) %>%
  layer_dropout(rate = 0.2) %>%
  layer_dense(units = 1000, kernel_regularizer = regularizer_l2(0.001), activation = 'tanh') %>%
  layer_dropout(rate = 0.2) %>%
