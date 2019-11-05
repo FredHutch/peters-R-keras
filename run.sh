62814cb3009d9154098b1824b5bccd1f0d8feb5a
Minta821 <minta82@gmail.com>
2019-11-05 14:01:31 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,17 +17,17 @@ git clone https://github.com/dtenenba/peters-R-keras.git
 
 cd peters-R-keras
 
-aws s3 cp $X_TRAIN_S3_URL ./x_train.csv
+aws s3 cp $X_TRAIN_S3_URL ./train_gwass.txt
 #aws s3 cp $X_TEST_S3_URL ./x_test.csv
-aws s3 cp $X_TEST_uk_S3_URL ./test_uk.csv
+aws s3 cp $X_TEST_uk_S3_URL ./train_gwass.txt
 #aws s3 cp $X_TEST_plco_S3_URL ./test_plco.csv
 #aws s3 cp model_reg.hdf5 $MODEL_OUTPUT_S3_URL
 if [ -z ${USE_PYTHON+x} ];
 then
     echo USE_PYTHON is not set, using R
     time R -f fitmodel.R
-    aws s3 cp history.reg.pdf $PDF_OUTPUT_S3_URL
-    aws s3 cp model_reg.hdf5 $MODEL_OUTPUT_S3_URL
+    #aws s3 cp history.reg.pdf $PDF_OUTPUT_S3_URL
+    #aws s3 cp model_reg.hdf5 $MODEL_OUTPUT_S3_URL
 else
     echo USE_PYTHON is set, using python
     time python fitmodel.py
