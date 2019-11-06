8d291ff2f8827cf3e583d4d8ee89f48d7954019a
Minta821 <minta82@gmail.com>
2019-11-06 15:44:01 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -20,7 +20,7 @@ test_uk=data.frame(test_uk)
 model <- keras_model_sequential()
 fscore=matrix(0,length(y_uk),1)
 model %>%
- layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.001), activation = 'relu', input_shape = c(ncol(x_train))) %>%
+ layer_dense(units = 128, kernel_regularizer = regularizer_l2(0.001), activation = 'relu', input_shape = c(ncol(x_train))) %>%
  layer_dropout(rate = 0.2) %>%
  layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.001), activation = 'relu') %>%
  layer_dropout(rate = 0.2) %>%
