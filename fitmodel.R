ecca4da2136420dd8043b83f6b5559433553f47e
Minta821 <minta82@gmail.com>
2019-11-11 19:22:46 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -114,7 +114,7 @@ layer_dropout(rate = 0.001) %>%
   model1 %>% compile(
     loss = 'binary_crossentropy',
  #optimizer = optimizer_rmsprop(lr=0.001),
- optimizer = optimizer_adam(lr=0.0001),
+ optimizer = optimizer_adam(lr=0.01),
  metrics = c('accuracy')
  #metrics = c(metric_auc)
 )
