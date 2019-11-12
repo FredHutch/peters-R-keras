c24ca8e651c31630a0fd94374af308ec515062d1
Minta821 <minta82@gmail.com>
2019-11-12 11:07:09 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -62,7 +62,7 @@ test_uk <- as.matrix(test_uk)
 parallel_model  %>% compile(
   loss = 'binary_crossentropy',
  #optimizer = optimizer_rmsprop(lr=0.001),
- optimizer = optimizer_adam(lr=0.001),
+ optimizer = optimizer_adam(lr=0.005),
  metrics = c('accuracy')
  #metrics = c(metric_auc)
 )
@@ -114,7 +114,7 @@ layer_dropout(rate = 0.001) %>%
   model1 %>% compile(
     loss = 'binary_crossentropy',
  #optimizer = optimizer_rmsprop(lr=0.001),
- optimizer = optimizer_adam(lr=0.001),
+ optimizer = optimizer_adam(lr=0.005),
  metrics = c('accuracy')
  #metrics = c(metric_auc)
 )
