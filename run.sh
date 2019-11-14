09fc2db0523ee541a8c24ceab73b6bf6e8b43b5a
Minta821 <minta82@gmail.com>
2019-11-14 12:10:46 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ git clone https://github.com/dtenenba/peters-R-keras.git
 
 cd peters-R-keras
 
-aws s3 cp $X_TRAIN_S3_URL ./train_gwass.txt
+aws s3 cp $X_TRAIN_S3_URL ./train_full.txt
 #aws s3 cp $X_TEST_S3_URL ./x_test.csv
 aws s3 cp $X_TEST_S3_URL ./eur_crc_test.txt
 #aws s3 cp $X_TEST_plco_S3_URL ./test_plco.csv
