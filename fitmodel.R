d3b6309e084b0196b8ed571c01bf2d4ab039eb78
Minta821 <minta82@gmail.com>
2019-12-09 10:44:46 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -55,7 +55,7 @@ parallel_model  %>% compile(
 
  parallel_model %>% fit(
 x_train, y_train,
-epochs = 10,batch_size=1000,
+epochs = 10,batch_size=128,
 )
 
   score=parallel_model %>% predict(test_uk,batch_size=128)
