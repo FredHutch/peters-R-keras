63401d571143a64b509007b79b584241c8c494e2
Minta821 <minta82@gmail.com>
2019-12-09 16:04:26 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -33,8 +33,8 @@ dim(test_uk) <- c(dim(test_uk),1)
 parallel_model %>% 
  layer_conv_1d(filters = 64, kernel_size = 1, activation = 'relu',kernel_regularizer = regularizer_l2(0.0001),
                 input_shape = c(ncol(x_train),1)) %>% 
-model.add(MaxPooling2D((2, 2)))%>% 
-model.add(Flatten())%>% 
+layer_max_pooling_1d(pool_size = 2) %>%
+layer_flatten()%>% 
 layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu', input_shape = c(ncol(x_train))) %>%
 layer_dropout(rate = 0.001) %>%
 layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
