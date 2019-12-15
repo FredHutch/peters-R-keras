c0087112680f7528948a429866f0316b3bba2dd9
Minta821 <minta82@gmail.com>
2019-12-14 16:52:38 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -52,10 +52,6 @@ layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation 
 layer_dropout(rate = 0.001) %>%
 layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
 layer_dropout(rate = 0.001) %>%
-layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
-layer_dropout(rate = 0.001) %>%
-layer_dense(units = 32, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
-layer_dropout(rate = 0.001) %>%
 layer_dense(units = 32, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
 layer_dropout(rate = 0.001) %>%
 layer_dense(units = 32, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
