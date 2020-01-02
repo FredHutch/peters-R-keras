e8c98b34981f2fe57754b3d0bb00b125c011debe
Minta821 <minta82@gmail.com>
2020-01-02 13:04:01 -0800

    Update fitmodel.R

--- fitmodel.R
+++ fitmodel.R
@@ -4,8 +4,8 @@ library(data.table)
 x_train=fread('train_full.txt')
 y_train=x_train$outcome
 
-x_train_hap=fread('train_hap_22.txt')
-y_train_hap=x_train_hap$outcome
+#x_train_hap=fread('train_hap_22.txt')
+#y_train_hap=x_train_hap$outcome
 
 
 y_train[y_train=='Case']=1
@@ -14,12 +14,12 @@ y_train[y_train=='Control']=0
 x_train$outcome=NULL
 x_train=data.frame(x_train)
 
-x_train_hap$outcome=NULL
-x_train_hap=data.frame(x_train_hap)
+#x_train_hap$outcome=NULL
+#x_train_hap=data.frame(x_train_hap)
 
 
-x_train_hap=x_train_hap[1:78164,]
-y_train_hap=y_train_hap[1:78164]
+#x_train_hap=x_train_hap[1:78164,]
+#y_train_hap=y_train_hap[1:78164]
 
 get.gpu.count <- function() {
   out <- system2("nvidia-smi", "-L", stdout=TRUE)
@@ -38,7 +38,7 @@ test_uk=test_uk[,idx]
 
 
 model <- keras_model_sequential()
-model1 <- keras_model_sequential()
+#model1 <- keras_model_sequential()
 #model2 <- keras_model_sequential()
 #model3 <- keras_model_sequential()
 #model4 <- keras_model_sequential()
@@ -136,41 +136,41 @@ parallel_model %>% fit(
 score=parallel_model %>% predict(test_uk,batch_size=128)
 
 
-test_uk_hap=fread('rpgeh_hap_22.txt')
-y_uk_hap=y_uk
-test_uk_hap=data.frame(test_uk_hap)
-idx=match(colnames(x_train_hap),colnames(test_uk_hap))
-test_uk_hap=test_uk_hap[,idx]
+#test_uk_hap=fread('rpgeh_hap_22.txt')
+#y_uk_hap=y_uk
+#test_uk_hap=data.frame(test_uk_hap)
+#idx=match(colnames(x_train_hap),colnames(test_uk_hap))
+#test_uk_hap=test_uk_hap[,idx]
 
-x_train <- as.matrix(x_train_hap)
-dim(x_train) <- c(dim(x_train),1)
-test_uk <- as.matrix(test_uk_hap)
-dim(test_uk) <- c(dim(test_uk),1)
+#x_train <- as.matrix(x_train_hap)
+#dim(x_train) <- c(dim(x_train),1)
+#test_uk <- as.matrix(test_uk_hap)
+#dim(test_uk) <- c(dim(test_uk),1)
 
-model1 %>%
- layer_conv_1d(filters = 64, kernel_size = 1, activation = 'relu',kernel_regularizer = regularizer_l2(0.0001),
+#model1 %>%
+# layer_conv_1d(filters = 64, kernel_size = 1, activation = 'relu',kernel_regularizer = regularizer_l2(0.0001),
                 input_shape = c(ncol(x_train),1)) %>%
-# layer_max_pooling_1d(pool_size = 2) %>%
- layer_flatten()%>%
- layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
- layer_dropout(rate = 0.001) %>%
- layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
- layer_dropout(rate = 0.001) %>%
- layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
- layer_dropout(rate = 0.001) %>%
- layer_dense(units = 1, activation = 'sigmoid')
+## layer_max_pooling_1d(pool_size = 2) %>%
+# layer_flatten()%>%
+# layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
+# layer_dropout(rate = 0.001) %>%
+# layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
+# layer_dropout(rate = 0.001) %>%
+# layer_dense(units = 64, kernel_regularizer = regularizer_l2(0.0001), activation = 'relu') %>%
+# layer_dropout(rate = 0.001) %>%
+ #layer_dense(units = 1, activation = 'sigmoid')
 ####Model
-parallel_model <- multi_gpu_model(model1, gpus=get.gpu.count())
-parallel_model  %>% compile(
-loss = 'binary_crossentropy',
-optimizer = optimizer_adam(lr=0.001),
-metrics = c('accuracy')
-)
-parallel_model %>% fit(
-  x_train, y_train,
-  epochs = 10,batch_size=7000,
-)
-score1=parallel_model %>% predict(test_uk,batch_size=128)
+#parallel_model <- multi_gpu_model(model1, gpus=get.gpu.count())
+#parallel_model  %>% compile(
+#loss = 'binary_crossentropy',
+#optimizer = optimizer_adam(lr=0.001),
+#metrics = c('accuracy')
+#)
+#parallel_model %>% fit(
+#  x_train, y_train,
+ # epochs = 10,batch_size=7000,
+#)
+#score1=parallel_model %>% predict(test_uk,batch_size=128)
 
-fwrite(data.frame(score,score1,y_uk),'score.csv')
+fwrite(data.frame(score,y_uk),'score.csv')
 
